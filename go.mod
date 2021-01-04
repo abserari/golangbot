713fb8033f45d5c769edbf1324cdf34f4fa0b0d9
AbserAri <abserari@gmail.com>
2021-01-04 10:24:17 +0800

    fill will last commit

--- go.mod
+++ go.mod
@@ -6,6 +6,9 @@ require (
 	github.com/NateScarlet/pixiv v0.4.1
 	github.com/everpcpc/pixiv v0.0.0-20201024033649-2be7b492c852
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+	github.com/k0kubun/pp v3.0.1+incompatible
+	github.com/spf13/viper v1.7.1
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
-	google.golang.org/grpc v1.28.0 // indirect
+	github.com/xelaj/go-dry v0.0.0-20201104165138-61a25872c05a
+	github.com/xelaj/mtproto v0.0.0-20201129002337-2c42bdacb467
 )
---
+++ mtproto/auth/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/k0kubun/pp"
+	"github.com/xelaj/go-dry"
+	"github.com/xelaj/mtproto"
+	"github.com/xelaj/mtproto/telegram"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("second argument must be phone number!")
+		os.Exit(1)
+	}
+	phoneNumber := os.Args[1]
+
+	// edit these params for you!
+	client, err := telegram.NewClient(telegram.ClientConfig{
+		// where to store session configuration. must be set
+		SessionFile: "./session1.json",
+		// host address of mtproto server. Actually, it can'be mtproxy, not only official
+		ServerHost: "149.154.167.50:443",
+		// public keys file is patrh to file with public keys, which you must get from https://my.telelgram.org
+		PublicKeysFile: "./auth_bot/keys.pem",
+		AppID:          94575,                              // app id, could be find at https://my.telegram.org
+		AppHash:        "a3406de8d171bb422bb6ddf3bbd800e2", // app hash, could be find at https://my.telegram.org
+	})
+	dry.PanicIfErr(err)
+
+	setCode, err := client.AuthSendCode(&telegram.AuthSendCodeParams{
+		phoneNumber, 94575, "a3406de8d171bb422bb6ddf3bbd800e2", &telegram.CodeSettings{},
+	})
+	dry.PanicIfErr(err)
+	pp.Println(setCode)
+
+	fmt.Print("Auth code:")
+	code, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	code = strings.ReplaceAll(code, "\n", "")
+
+	auth, err := client.AuthSignIn(&telegram.AuthSignInParams{
+		PhoneNumber:   phoneNumber,
+		PhoneCodeHash: setCode.PhoneCodeHash,
+		PhoneCode:     code,
+	})
+	if err == nil {
+		pp.Println(auth)
+
+		fmt.Println("Success! You've signed in!")
+		return
+	}
+
+	// if you don't have password protection — THAT'S ALL! You're already logged in.
+	// but if you have 2FA, you need to make few more steps:
+
+	// could be some errors:
+
+	mtError, ok := errors.Unwrap(err).(*mtproto.ErrResponseCode)
+	// SESSION_PASSWORD_NEEDED says that your account has 2FA protection
+	if !ok || mtError.Message != "SESSION_PASSWORD_NEEDED" {
+		fmt.Println("SignIn failed:", err)
+		println("\n\nMore info about error:")
+		pp.Println(err)
+		return
+	}
+
+	fmt.Print("Password:")
+	password, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	password = strings.ReplaceAll(password, "\n", "")
+
+	accountPassword, err := client.AccountGetPassword()
+	dry.PanicIfErr(err)
+
+	// GetInputCheckPassword is fast response object generator
+	inputCheck, err := telegram.GetInputCheckPassword(password, accountPassword)
+	dry.PanicIfErr(err)
+
+	auth, err = client.AuthCheckPassword(&telegram.AuthCheckPasswordParams{
+		Password: inputCheck,
+	})
+	dry.PanicIfErr(err)
+
+	pp.Println(auth)
+	fmt.Println("Success! You've signed in!")
+}
